75a578d0490a1fd0ab941eb987ee00dba8ec0e2b
liwei <liwei@jike.com>
2013-01-14 21:51:58 +0800

    update minhash

--- minhash.py
+++ minhash.py
@@ -1,53 +1,52 @@
 #!/usr/bin/python
 
 import random
-import hashlib
-import pyhash
 
-def h_fnv1_32(key):
-    return pyhash.fnv1_32()(key)
-def h_murmur3(key):
-    return pyhash.murmur3_32()(key)
-def h_lookup3(key):
-    return pyhash.lookup3()(key)
-def h_superfasthash(key):
-    return pyhash.super_fast_hash()(key)
-def h_md5(key):
-    h = hashlib.md5()
-    h.update(key)
-    return long(h.hexdigest(), 16)
-def h_sha1(key):
-    h = hashlib.sha1()
-    h.update(key)
-    return long(h.hexdigest(), 16)
-def h_sha224(key):
-    h = hashlib.sha224()
-    h.update(key)
-    return long(h.hexdigest(), 16)
-def minhash_map(a_set):
-    '''mapping a set to a smaller space with minhash'''
-    hash_func = [h_md5, h_sha1, h_superfasthash, h_murmur3, h_fnv1_32, h_lookup3,
-                 h_sha224]
-    ret = []
-    for func in hash_func:
-        ret.append(min([func(v) for v in a_set]))
+def next_highest(v):
+    v-=1
+    v |= v >> 1
+    v |= v >> 2
+    v |= v >> 4
+    v |= v >> 8
+    v |= v >>16
+    v+=1
+    return v
 
-    return ret
+def naive_sim(set1, set2):
+    return float(len(set1.intersection(set2))) / len(set1.union(set2))
+class MinHash(object):
+    def __init__(self, init_hash_func, universe_size):
+        self.hash_funcs = []
+        self.universe_size = universe_size
+        self.hash_size = next_highest(universe_size) - 1
+        for i in range(init_hash_func):
+            self.add_hash_func();
+        self.min_hash_sets = []
+    def add_hash_func(self):
+        a = random.randint(self.universe_size)
+        b = random.randint(self.universe_size)
+        c = random.randint(self.universe_size)
+        def qhash(x):
+            int_x = int(x)
+            return abs((a * (int_x >> 4) + b *x + c) & self.hash_size)
+        self.hash_funcs.append(qhash)
+        for offset, min_hash_struct in enumerate(self.min_hash_sets):
+            the_set, hash_values = min_hash_struct["set"], min_hash_struct["v"]
+            hash_values.append(self.apply_hash(self.hash_funcs[-1], the_set))
 
-def sim(set1_mapped, set2_mapped):
-    '''calculate the similarity betweet two mapped set(using minhash'''
-    same = 0
-    for v1, v2 in zip(set1_mapped, set2_mapped):
-        if v1 == v2:
-            same += 1
-    return float(same) / len(set1_mapped)
+    def add_set(self, the_set):
+        hash_values = []
+        for hash_func in self.hash_funcs:
+            hash_values.append(self.apply_hash(hash_func, the_set))
+        
+        self.min_hash_sets.append({
+            "set": the_set,
+            "v": hash_values
+        })
+    def apply_hash(self, hash_func, the_set):
+        min_v = self.hash_size
+        for v in the_set:
+            min_v = min(hash_func(v), min_v)
 
-if __name__ == '__main__':
-    set1 = set()
-    set2 = set()
-    for i in xrange(100):
-        set1.add(str(random.randint(0, 200)))
-        set2.add(str(random.randint(0, 200)))
-    set1_mapped = minhash_map(set1)
-    set2_mapped = minhash_map(set2)
-    print sim(set1_mapped, set2_mapped)
+    def cal_simmilarity(self, min_hash_set1, min_hash_set2):
+        return naive_sim(min_hash_set1["v"], min_hash_set2["v"])
