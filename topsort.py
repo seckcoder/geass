f3629a686680bb427be187c9308b199723832339
liwei <liwei@jike.com>
2013-05-06 15:10:03 +0800

    update

--- topsort.py
+++ topsort.py
@@ -1,54 +1,186 @@
-from graph import Graph
 from collections import defaultdict
+class DependencyGraph(object):
+    """
+    The code is from Celery.
+    
+    A directed acyclic graph of objects and their dependencies.
 
-class TopGraph(Graph):
+    Supports a robust topological sort
+    to detect the order in which they must be handled.
+
+    Takes an optional iterator of ``(obj, dependencies)``
+    tuples to build the graph from.
+
+    .. warning::
+
+        Does not support cycle detection.
+
+    """
+
+    def __init__(self, it=None):
+        self.adjacent = {}
+        if it is not None:
+            self.update(it)
+
+    def add_arc(self, obj):
+        """Add an object to the graph."""
+        self.adjacent.setdefault(obj, [])
+
+    def add_edge(self, A, B):
+        """Add an edge from object ``A`` to object ``B``
+        (``A`` depends on ``B``)."""
+        self[A].append(B)
 
     def topsort(self):
-        return self._khan62()
+        """Sort the graph topologically.
+
+        :returns: a list of objects in the order
+            in which they must be handled.
+
+        """
+        graph = DependencyGraph()
+        components = self._tarjan72()
+
+        NC = dict((node, component)
+                  for component in components
+                  for node in component)
+        for component in components:
+            graph.add_arc(component)
+        for node in self:
+            node_c = NC[node]
+            for successor in self[node]:
+                successor_c = NC[successor]
+                if node_c != successor_c:
+                    graph.add_edge(node_c, successor_c)
+        return [t[0] for t in graph._khan62()]
+
+    def valency_of(self, obj):
+        """Returns the velency (degree) of a vertex in the graph."""
+        try:
+            l = [len(self[obj])]
+        except KeyError:
+            return 0
+        for node in self[obj]:
+            l.append(self.valency_of(node))
+        return sum(l)
+
+    def update(self, it):
+        """Update the graph with data from a list
+        of ``(obj, dependencies)`` tuples."""
+        tups = list(it)
+        for obj, _ in tups:
+            self.add_arc(obj)
+        for obj, deps in tups:
+            for dep in deps:
+                self.add_edge(obj, dep)
+
+    def edges(self):
+        """Returns generator that yields for all edges in the graph."""
+        return (obj for obj, adj in self.iteritems() if adj)
 
     def _khan62(self):
-        count = defaultdict(lambda :0)
+        """Khans simple topological sort algorithm from '62
+
+        See http://en.wikipedia.org/wiki/Topological_sorting
+
+        """
+        count = defaultdict(lambda: 0)
+        result = []
+
         for node in self:
             for successor in self[node]:
                 count[successor] += 1
-    
         ready = [node for node in self if not count[node]]
-
-        result = []
 
         while ready:
             node = ready.pop()
             result.append(node)
+
             for successor in self[node]:
                 count[successor] -= 1
                 if count[successor] == 0:
                     ready.append(successor)
-        
-        # If there's no cycles in the graph, then count will all be zero
-        return result.reverse()
+        result.reverse()
+        return result
 
-graph = TopGraph()
+    def _tarjan72(self):
+        """Tarjan's algorithm to find strongly connected components.
 
-graph.add_arc(2)
-graph.add_arc(3)
-graph.add_arc(5)
-graph.add_arc(7)
-graph.add_arc(8)
-graph.add_arc(9)
-graph.add_arc(10)
-graph.add_arc(11)
+        # NOTE(by seckcoder) : I still can't fully understand the algorithm...
 
-graph.add_edge(3, 8)
-graph.add_edge(3, 10)
-graph.add_edge(8, 9)
-graph.add_edge(7, 11)
-graph.add_edge(5, 11)
-graph.add_edge(11, 9)
-graph.add_edge(11, 2)
-graph.add_edge(11, 10)
-#graph.add_edge(9, 7)
+        See http://bit.ly/vIMv3h.
 
-#graph.add_edge(2,3)
-#graph.add_edge(3,5)
+        """
+        result, stack, low = [], [], {}
 
-graph.topsort()
+
+        def visit(node):
+            # NOTE(by seckcoder) : For every visit, it do a dfs search,
+            # and return a subtree
+
+            if node in low:
+                return
+            num = len(low)
+            low[node] = num
+            # NOTE(by seckcoder) : low records the lowest index the node can reached.
+            stack_pos = len(stack)
+            stack.append(node)
+
+            for successor in self[node]:
+                visit(successor)
+                low[node] = min(low[node], low[successor])
+
+            if num == low[node]:
+                component = tuple(stack[stack_pos:])
+                stack[stack_pos:] = []
+                result.append(component)
+                for item in component:
+                    low[item] = len(self)
+
+        for node in self:
+            visit(node)
+
+        return result
+
+    def to_dot(self, fh, ws=' ' * 4):
+        """Convert the graph to DOT format.
+
+        :param fh: A file, or a file-like object to write the graph to.
+
+        """
+        fh.write('digraph dependencies {\n')
+        for obj, adjacent in self.iteritems():
+            if not adjacent:
+                fh.write(ws + '"%s"\n' % (obj, ))
+            for req in adjacent:
+                fh.write(ws + '"%s" -> "%s"\n' % (obj, req))
+        fh.write('}\n')
+
+    def __iter__(self):
+        return iter(self.adjacent)
+
+    def __getitem__(self, node):
+        return self.adjacent[node]
+
+    def __len__(self):
+        return len(self.adjacent)
+
+    def __contains__(self, obj):
+        return obj in self.adjacent
+
+    def _iterate_items(self):
+        return self.adjacent.iteritems()
+    items = iteritems = _iterate_items
+
+    def __repr__(self):
+        return '\n'.join(self.repr_node(N) for N in self)
+
+    def repr_node(self, obj, level=1):
+        output = ['%s(%s)' % (obj, self.valency_of(obj))]
+        if obj in self:
+            for other in self[obj]:
+                d = '%s(%s)' % (other, self.valency_of(other))
+                output.append('     ' * level + d)
+                output.extend(self.repr_node(other, level + 1).split('\n')[1:])
+        return '\n'.join(output)
+
